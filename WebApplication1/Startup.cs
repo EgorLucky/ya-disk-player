fb315a6849b9d27e6d5a4de2d66c7df5c223a60c
DESKTOP-4CDNPFM\Egor <luk_96@mail.ru>
2021-05-26 22:28:27 +0300

    update sln file and Startup file

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
