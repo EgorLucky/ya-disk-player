e507977363569068ca1ce07aadda5c7358e62a29
DESKTOP-4CDNPFM\Egor <luk_96@mail.ru>
2021-05-26 22:25:12 +0300

    add project for yandex disk player api

---
+++ YandexDiskPlayerLibrary/YandexDiskPlayerApi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+
+namespace YandexDiskPlayerLibrary
+{
+    public class YandexDiskPlayerApi
+    {
+        protected string _accessToken;
+        protected readonly HttpClient _client;
+        public string AccessToken { set { _accessToken = value; } }
+
+        public YandexDiskPlayerApi(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public YandexDiskPlayerApi(HttpClient client, string accessToken)
+        {
+            _accessToken = accessToken;
+            _client = client;
+        }
+
+
+    }
+}
