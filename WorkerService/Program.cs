f0a4b641a376c06e441d2828d7927271256b5600
luk_96 <luk_96@mail.ru>
2021-06-04 15:40:25 +0300

    add workerService project

---
+++ WorkerService/Program.cs
@@ -0,0 +1,52 @@
+using Implementations.Mq;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WorkerService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddMassTransit(x =>
+                    {
+                        var rabbitMqConfigJson = Environment.GetEnvironmentVariable("yadplayerRabbitMqConfig");
+                        var RabbitMqConfig = JsonSerializer.Deserialize<RabbitMqConfig>(rabbitMqConfigJson);
+
+                        x.AddConsumer<YandexDiskPlayerSynchronizationConsumer>();
+                        x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(config => 
+                        {
+                            config.Host(RabbitMqConfig.Host, RabbitMqConfig.VirtualHost, h =>
+                            {
+                                h.Username(RabbitMqConfig.Username);
+                                h.Password(RabbitMqConfig.Password);
+
+                            });
+
+                            config.ReceiveEndpoint("YandexDiskPlayerSynchronization", ep =>
+                            {
+                                ep.PrefetchCount = 16;
+                                //ep.UseMessageRetry(r => r.Interval(2, 100));
+                                ep.ConfigureConsumer<YandexDiskPlayerSynchronizationConsumer>(provider);
+                            });
+                        }));
+                    });
+                    services.AddMassTransitHostedService(true);
+
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
---
+++ WorkerService/YandexDiskPlayerSynchronizationConsumer.cs
@@ -0,0 +1,25 @@
+﻿using Implementations.Mq;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace WorkerService
+{
+    public class YandexDiskPlayerSynchronizationConsumer :
+        IConsumer<YandexDiskPlayerSynchronization>
+    {
+        readonly ILogger<YandexDiskPlayerSynchronizationConsumer> _logger;
+
+        public YandexDiskPlayerSynchronizationConsumer(ILogger<YandexDiskPlayerSynchronizationConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<YandexDiskPlayerSynchronization> context)
+        {
+            _logger.LogInformation("Received Text: {Text}", context.Message.Id);
+
+            return Task.CompletedTask;
+        }
+    }
+}
