b85194e7fb00f772ca493765f2f68e960bf24239
luk_96 <luk_96@mail.ru>
2021-06-16 13:11:00 +0300

    add GetIgnorePathsRequestModel

---
+++ DomainLogic/RequestModels/GetIgnorePathsRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLogic.RequestModels
+{
+    public record GetIgnorePathsRequestModel(
+        int Take, 
+        int Page, 
+        string Search,
+        List<string> Paths, 
+        string YandexUserId
+    );
+}
